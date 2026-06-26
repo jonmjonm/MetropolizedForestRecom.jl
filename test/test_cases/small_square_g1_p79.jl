fd6a578de2c576d40ef8e15e5bce0ae314dcbf78
Gregory Herschlag <gjh@math.duke.edu>
2026-06-26 13:22:24 -0400

    update

--- test/test_cases/small_square_g1_p79.jl
+++ test/test_cases/small_square_g1_p79.jl
@@ -2,7 +2,7 @@
 
     constraints = initialize_constraints()
     add_constraint!(constraints, PopulationConstraint(7,9))
-    n = 10000
+    n = 20000
     measure = Measure(1.0)
 
     observed_districts = get_observed_districts(small_square_graph, constraints, small_square_dists, measure, n);
